da2ebafc94ea233a6a660d8e38f6691b6b91209b
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-09-04 19:07:40 +0300

    chore: set up ESLint as per RS School requirements

--- rest-client/eslint.config.mjs
+++ rest-client/eslint.config.mjs
@@ -1,6 +1,12 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactCompiler from "eslint-plugin-react-compiler";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +17,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  js.configs.recommended,
+  ...tseslint.configs.strict,
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +27,24 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-compiler": reactCompiler,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "react-compiler/react-compiler": "error",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
   },
 ];
 
