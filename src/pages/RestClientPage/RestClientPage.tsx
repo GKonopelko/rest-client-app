452e7b4737647b8de5855991bcb9b6e3cd86d9ae
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-09-09 22:08:46 +0300

    feat: add url line

--- src/pages/RestClientPage/RestClientPage.tsx
+++ src/pages/RestClientPage/RestClientPage.tsx
@@ -1,5 +1,41 @@
+'use client';
+
+import { Card, Typography, Space, Input, Button, Select } from 'antd';
 import styles from './RestClientPage.module.css';
 
+const { Title } = Typography;
+const { Option } = Select;
+const methods: string[] = [
+  'GET',
+  'POST',
+  'PUT',
+  'DELETE',
+  'PATCH',
+  'HEAD',
+  'OPTIONS',
+  'CONNECT',
+  'TRACE',
+];
+
 export default function RestClientPage() {
-  return <div className={styles.page}>Rest Client Page</div>;
+  return (
+    <section className={styles.section}>
+      <Card className={styles.card}>
+        <Title level={2} className={styles.title}>
+          Test Your Request
+        </Title>
+        <Space.Compact className={styles['url-line']} size="large">
+          <Select className={styles.select} defaultValue="GET">
+            {methods.map((elem) => (
+              <Option key={elem} value={elem}>
+                {elem}
+              </Option>
+            ))}
+          </Select>
+          <Input placeholder="Enter base URL" />
+          <Button type="primary">Submit</Button>
+        </Space.Compact>
+      </Card>
+    </section>
+  );
 }
