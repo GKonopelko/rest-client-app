3bb5ba33346b1a97d42aa256c3dd8937836cc8cd
Forlocks <volgaopa@gmail.com>
2025-09-15 21:36:12 +0300

    fix: change import variant

--- src/app/[locale]/sign-in/page.tsx
+++ src/app/[locale]/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import SignInPage from '../../../pages/SignInPage/SignInPage';
+import SignInPage from '@/pages/SignInPage/SignInPage';
 
 export default function AppSignInPage() {
   return <SignInPage />;
